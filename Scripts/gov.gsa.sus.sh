348e65f0a9ea9465e78ac538d67917e796748bcb
John Graphia <jgraphia@mac.com>
2018-05-05 22:06:18 -0500

    Overhaul

--- Scripts/gov.gsa.sus.sh
+++ Scripts/gov.gsa.sus.sh
@@ -4,25 +4,24 @@ bddy=/usr/libexec/PlistBuddy
 tgt=/Library/Preferences/gov.gsa.sus.plist
 #
 # Cleanup from previous Day
-$bddy -c "delete Updates" $tgt
+$bddy -c "delete Updates" $tgt &> /dev/null
 #
-$bddy -c "add Updates array " $tgt
+$bddy -c "add Updates array " $tgt &> /dev/null
 #
 #
 for lst in $(softwareupdate -l|sed -e '1,4 d' -e '/*/d' -e 's/ //g')
 do
-$bddy -c "add Updates:0 string "$lst"" $tgt
+$bddy -c "add Updates:0 string "$lst"" $tgt &> /dev/null
 #
 done
 #
 UpdCnt=`defaults read /Library/Preferences/gov.gsa.sus Updates|sed -e 's/(//g' -e 's/)//g' -e '/^$/d'|wc -l`
 #
 #
-$bddy -c "add UpdateCount integer '$UpdCnt' " $tgt
+$bddy -c "add UpdateCount integer '$UpdCnt' " $tgt &> /dev/null
 #
-$bddy -c "add DeferDays integer 8 " $tgt
-$bddy -c "add DeferCount integer 0 " $tgt
+$bddy -c "add DeferDays integer 8 " $tgt &> /dev/null
+$bddy -c "add DeferCount integer 0 " $tgt &> /dev/null
 
 
 exit 0
-
--- Scripts/gov.gsa.updialog.sh
+++ Scripts/gov.gsa.updialog.sh
@@ -6,42 +6,51 @@ icon="/usr/local/GSAfiles/GSA-logo_blue.icns"
 #
 heading="Required Software Updates Count"
 #
-description="There are"
+description="There are "
 # updcount=`sofwtwareupdate -l|sed -e '/*/d' -e '1,4 d'|wc -l`|sed 's/^[ \t]*//'
 # test code
 updcount=`/usr/bin/defaults read $tgt UpdateCount `
 #
-d2="Security updates for your machine. If you do nothing the window will timeout in 10 seconds and install updates."
+d2="Security updates for your machine. If you do nothing this window will timeout in 15 minutes and updates will be installed."
+d3="You can defer for"
+d4=`/usr/bin/defaults read $tgt DeferDays `
+d5="more days. Updates will be force installed on the final day and your machine will be rebooted. Please use Self-Service to perform updates and restart at your convenience."
 #
-result=`"$helper" -windowType hud -lockHUD -title "$heading" -alignHeading center -icon "$icon" -iconSize 96 -description "$description$updcount $d2" -button1 "Install" -button2 "Defer" -timeout 900 -countdown -alignCountdown left`
+result=`"$helper" -windowType hud -lockHUD -title "$heading" -alignHeading center -icon "$icon" -iconSize 96 -description "$description$updcount $d2 $d3 $d4 $d5" -button1 "Install" -button2 "Defer" -timeout 900 -countdown -alignCountdown left`
 if [ $result == 0 ]; then
 echo "I will Install Updates"
+# sleep 900
 #softwareupdate -i -a
-# We should probably think about destroying gov.gsa.sus.plist at this time
-# That way machine reboots and upon next andy run gets a new list of updates. This would also reset the defer counter
-## workflow
-
+#
 # if grep "restart" then reboot with 15 minute timer and dialog no defer of reboot
 #
 else [ $result == 2 ];
-echo "I will defer installation"
+# echo "User defered installation" to GSA log
 # Increment Defer Counter
 dfrcnt=`$bddy -c "print :DeferCount" $tgt`
 inc=$(($dfrcnt+1))
 $bddy -c "set :DeferCount $inc " $tgt
 #
+fi
 #
-# Decement Defer Day counter
+# Decrement Defer Day counter
 dfrday=`$bddy -c "print :DeferDays" $tgt`
+if [ $dfrday == 0 ]; then
+#  nudge every 3 days, X number of update are avail,
+final="You can no longer defer. Updates will be installed and your system rebooted in 15 minutes"
+result=`"$helper" -windowType hud -lockHUD -title "$heading" -alignHeading center -icon "$icon" -iconSize 96 -description "$final" -button1 "Install" -timeout 900 -countdown -alignCountdown left`
+else
 dec=$(($dfrday-1))
-$bddy -c "set :DeferDay $dec " $tgt
+$bddy -c "set :DeferDays $dec " $tgt
 #
 fi
 #
-# if [$dfrday == 5]
-# then
-#  nudge every 3 days, X number of update are avail, You have can defer for X number of days. Updates will be force installed in X days
-# Go to Self-Service to perform updates and restart at your convenience.
-# else
-# You can no longer defer. Updates will be installed and your system rebooted in 15 minutes
-# 
+#if [$dfrday == 0]; then
+##  nudge every 3 days, X number of update are avail,
+#final="You can no longer defer. Updates will be installed and your system rebooted in 15 minutes"
+#result=`"$helper" -windowType hud -lockHUD -title "$heading" -alignHeading center -icon "$icon" -iconSize 96 -description "$final" -button1 "Install" -timeout 900 -countdown -alignCountdown left`
+#else
+#exit 0
+##
+#fi
+#
