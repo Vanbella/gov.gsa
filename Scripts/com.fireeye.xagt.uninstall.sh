7932fab93ab12f7d5768637a10a468e5d8d31f7f
John Graphia <jgraphia@mac.com>
2018-05-08 07:10:34 -0500

    Added FireEye HX uninstall script

---
+++ Scripts/com.fireeye.xagt.uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  com.fireeye.xagt.uninstall.sh
+#  gov.gsa
+#
+#  Created by John Graphia on 5/8/18.
+#  
+# Run the vendor uninstall script
+/Library/FireEye/xagt/uninstall.tool
+#
+exit 0
