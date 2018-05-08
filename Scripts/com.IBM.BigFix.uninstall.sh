8597cd26c2ccb388a73c0abdd962f9a9d05540c0
John Graphia <jgraphia@mac.com>
2018-05-08 07:39:34 -0500

    Added IBM BF agent uninstaller

---
+++ Scripts/com.IBM.BigFix.uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#  com.IBM.BigFix.uninstall.sh
+#  gov.gsa
+#
+#  Created by John Graphia on 5/8/18.
+#  
+# Use vendor provided uninstaller
+/Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentUninstaller.sh
+#
+exit 0
+
