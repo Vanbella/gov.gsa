370a3230cf4dcb8a0c92b961248127a9d3e72156
John Graphia <john.graphia@gsa.gov>
2018-06-28 13:29:57 -0500

    Update gov.gsa.sus.extattrib.sh
    re-write

--- ExtAttrib/gov.gsa.sus.extattrib.sh
+++ ExtAttrib/gov.gsa.sus.extattrib.sh
@@ -4,18 +4,15 @@
 #  jss
 #
 #  Created by John Graphia on 4/30/18.
-#
+#  Modified JFG 06/28/18
+#################################################################
 bddy=/usr/libexec/PlistBuddy
-tgt=/Library/Preferences/gov.gsa.sus.plist
+#sus=/Library/Preferences/gov.gsa.sus.plist
+tgt=/Users/johngraphia/Desktop/SotfwareUpdate.plist
+#################################################################
+crt=`$bddy -c "print :CriticalUpdateInstall" $tgt`
+lrua=`$bddy -c "print :LastRecommendedUpdatesAvailable" $tgt`
+lua=`$bddy -c "print :LastUpdatesAvailable" $tgt`
+echo echo "<result>Critical $crt Recommended $lrua  Last $lua</result>"
 #
-#
-UpdateCount=`$bddy -c "print UpdateCount" $tgt`
-# echo $UpdateCount
-if [ $UpdateCount -gt 0 ]; then
-#echo You have $UpdateCount updates to be installed.
-echo "<result>True</result>"
-else
-#echo You have no updates to be installed.
-echo "<result>False</result>"
-fi
- 
+exit 0
