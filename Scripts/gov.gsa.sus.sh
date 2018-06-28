46b0fee22c853ad3ee69f76db192324921101c43
John Graphia <john.graphia@gsa.gov>
2018-06-28 07:39:16 -0500

    re-write to accommodate com.apple.SoftwareUpdate.plist

--- Scripts/gov.gsa.sus.sh
+++ Scripts/gov.gsa.sus.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 #
 bddy=/usr/libexec/PlistBuddy
-tgt=/Library/Preferences/gov.gsa.sus.plist
+sus=/Library/Preferences/gov.gsa.sus.plist
+tgt=/Library/Preferences/com.apple.SoftwareUpdate.plist
 #
-# Cleanup from previous Day
+# Cleanup
 ##############################
-$bddy -c "delete Updates" $tgt &> /dev/null
-$bddy -c "add Updates array " $tgt &> /dev/null
-$bddy -c "delete UpdateCount" $tgt &> /dev/null
+$bddy -c "delete Updates array " $sus &> /dev/null
+$bddy -c "delete Updates" $sus &> /dev/null
+#$bddy -c "delete UpdateCount" $sus &> /dev/null
 ##############################
-dfrcnt=`$bddy -c "print :DeferCount" $tgt`
-if [[ $dfrcnt -gt 0 ]]; then
-echo "defer in process"
-else
-#$bddy -c "delete DeferDays" $tgt &> /dev/null
-$bddy -c "add DeferDays integer 8 " $tgt &> /dev/null
+#dfrcnt=`$bddy -c "print :DeferCount" $sus`
+#if [[ $dfrcnt -gt 0 ]]; then
+#echo "defer in process"
+#else
+#$bddy -c "delete DeferDays" $sus &> /dev/null
+$bddy -c "add DeferDays integer 8 " $sus &> /dev/null
 # Give the user 8 days to defer update installs
-#$bddy -c "delete DeferCount" $tgt &> /dev/null
-$bddy -c "add DeferCount integer 0 " $tgt &> /dev/null
+#$bddy -c "delete DeferCount" $sus &> /dev/null
+$bddy -c "add DeferCount integer 0 " $sus &> /dev/null
 # Count how many times the user defers
-fi
-#
-for lst in $(softwareupdate -l|sed -e '1,4 d' -e '/*/d' -e 's/ //g')
-do
-$bddy -c "add Updates:0 string "$lst"" $tgt &> /dev/null
-#
-done
-#
-UpdCnt=`defaults read /Library/Preferences/gov.gsa.sus Updates|sed -e 's/(//g' -e 's/)//g' -e '/^$/d'|wc -l`
-#
-#
-$bddy -c "add UpdateCount integer '$UpdCnt' " $tgt &> /dev/null
-#
+#fi
+# List the updates and write them to an array
+# superceded by com.apple.SoftwareUpdate.plist
+#for lst in $(softwareupdate -l|sed -e '1,4 d' -e '/*/d' -e 's/ //g')
+#do
+#$bddy -c "add Updates:0 string "$lst"" $tgt &> /dev/null
+#done
+#lrua=`defaults read $tgt LastRecommendedUpdatesAvailable`
+#lua=`defaults read $tgt LastUpdatesAvailable`
+#$bddy -c "add LastRecommendedUpdatesAvailable integer '$lrua' " $sus &> /dev/null
+#$bddy -c "add LastUpdatesAvailable integer '$lua' " $sus &> /dev/null
+
 exit 0
